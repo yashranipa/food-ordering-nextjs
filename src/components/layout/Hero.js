ab0010691ea5e6fc64b8d82c77be9f260c62fe62
yashranipa <ranipayash01@student.fdu.edu>
2024-01-02 23:16:04 -0800

    building template for hero section

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,14 +1,33 @@
 import Image from "next/image";
+import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section>
-      <h1>Everything is better with a Pizza</h1>
-      <p>
-        Pizza is a missing piece that makes every day complete, a simple yet
-        delicious joy in life
-      </p>
-      <div className="w-full h-max">
+    <section className="hero">
+      <div className="py-12">
+        <h1 className="text-4xl semibold">
+          Everything <br />
+          is better
+          <br />
+          with a &nbsp;
+          <span className="text-primary">Pizza</span>
+        </h1>
+        <p className="my-4 text-gray-500 text-sm">
+          Pizza is a missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div className="flex gap-4 text-sm">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
+            Order Now
+            <Right />
+          </button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+            Learn More
+            <Right />
+          </button>
+        </div>
+      </div>
+      <div className="w-16 h-16 relative">
         <Image
           src={"/pizza.png"}
           layout={"fill"}
