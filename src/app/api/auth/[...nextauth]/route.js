98f17e30cb39150176ee4b94f96d3cd3ae4d2305
yashranipa <ranipayash01@student.fdu.edu>
2024-01-21 11:01:51 -0800

    implemented login page and route

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { User } from "../../../models/User";
+
+const handler = NextAuth({
+  secret: process.env.SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@test.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
+        console.log({ passwordOk });
+        if (passwordOk) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
---
+++ src/app/login/page.js
@@ -0,0 +1,52 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginInProgress, setLoginInProgress] = useState(false);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    setLoginInProgress(true);
+    await signIn("credentials", { email, password });
+    setLoginInProgress(false);
+  };
+
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          disabled={loginInProgress}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={password}
+          disabled={loginInProgress}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit" disabled={loginInProgress}>
+          Login
+        </button>
+        <div className="my-4 text-center text-gray-500">
+          or Login with provider
+        </div>
+        <button className="flex gap-4 justify-center">
+          <Image src={"/google.png"} alt={""} width={24} height={24} />
+          Login with Google
+        </button>
+      </form>
+    </section>
+  );
+}
