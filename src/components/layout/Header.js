5979ec4f4b360082cbfcbacc81ce5dc346151187
yashranipa <ranipayash01@student.fdu.edu>
2024-01-21 11:20:37 -0800

    implemented logout functionality using SessionProvider

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,11 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Header() {
+  const session = useSession();
+  const status = session.status;
   return (
     <header className="flex items-center justify-between">
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
@@ -13,13 +18,25 @@ export default function Header() {
         <Link href={"#contact"}>Contact</Link>
       </nav>
       <nav className="flex items-center gap-8 text-gray-500 font-semibold">
-        <Link href={"/login"}>Login</Link>
-        <Link
-          href={"/register"}
-          className="bg-primary rounded-full text-white px-6 py-2"
-        >
-          Register
-        </Link>
+        {status === "authenticated" && (
+          <button
+            onClick={() => signOut()}
+            className="bg-primary rounded-full text-white px-6 py-2"
+          >
+            Logout
+          </button>
+        )}
+        {status === "unauthenticated" && (
+          <>
+            <Link href={"/login"}>Login</Link>
+            <Link
+              href={"/register"}
+              className="bg-primary rounded-full text-white px-6 py-2"
+            >
+              Register
+            </Link>
+          </>
+        )}
       </nav>
     </header>
   );
